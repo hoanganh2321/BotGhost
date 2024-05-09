7225c7a9f35a986c19335df2d562bedcde86b884
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-09 08:17:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Make by Kaioshin 🔥","https://discord.gg/XBHZUwqAzK"];
 
 
 let currentIndex = 0;
